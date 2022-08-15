faab1838bd819be7a3bc8d881f00d61c3f37f103
Let0Pda <a.gavrilov70@gmail.com>
2022-08-15 17:51:16 +0300

    Добавил обратную сортировку от max => min, и рандом

--- Exemple014_MinMax/Program.cs
+++ Exemple014_MinMax/Program.cs
@@ -4,7 +4,8 @@
 
 //int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 }; // определение  массива "[]", определение значений массива {}
 
-int[] arr = new int[44];
+/*
+int[] arr = new int[12];
 Random rand = new Random();
 for (int y = 0; y < arr.Length; y++)
 {
@@ -38,6 +39,48 @@ void SelectionSort(int[] array) // Метод сортировки
         int temporary = array[i];           //замена элементов местами
         array[i] = array[minPosition];
         array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+*/
+//int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+int[] arr = new int[25];
+Random rand = new Random();
+for (int y = 0; y < arr.Length; y++)
+{
+    arr[y] = rand.Next(-1000, 1000);
+}
+void PrintArray(int[] array)  // Метод вывода массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // Метод сортировки
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j; // поиск минимального элемента
+
+        }
+        int temporary = array[i];           //замена элементов местами
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 }
 
