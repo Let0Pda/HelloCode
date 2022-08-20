e6c5a1f1c76accecd93d511ae96378884b0c0bae
Let0Pda <a.gavrilov70@gmail.com>
2022-08-20 22:15:50 +0300

    Лекция 4, добавлены упр 15-18

---
+++ Exemple017_RecursionFactorial/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+double Factorial(int n) // Факториал числа обозначается "n!"
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
